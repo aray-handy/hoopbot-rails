f8cb15aa614f25af893875e636e103a0df72d166
Amortya Ray <aray@handy.com>
2019-02-06 09:43:24 -0500

    Process only when dialog_submission

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -1,14 +1,16 @@
 class SubmitController < ApplicationController
   def create
-    command_params = {
-      type: submission[:type],
-      start_date: submission[:start_date],
-      end_date: submission[:end_date],
-      user_name: parsed_payload[:user][:name],
-      response_url: response_url
-    }
+    if parsed_payload[:type] == "dialog_submission"
+      command_params = {
+        type: submission[:type],
+        start_date: submission[:start_date],
+        end_date: submission[:end_date],
+        user_name: parsed_payload[:user][:name],
+        response_url: response_url
+      }
 
-    CommandWorker.perform_async(command_params.to_h)
+      CommandWorker.perform_async(command_params.to_h)
+    end
 
     render json: {}, status: :ok
   end
