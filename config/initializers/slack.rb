7180df4f40eccd9637ed009a07f38cdbf9fbbb9f
Amortya Ray <aray@handy.com>
2019-02-07 10:07:26 -0500

    error if missing secret

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,2 +1,3 @@
 raise 'Missing ENV[SLACK_BOT_TOKEN]!' unless ENV["SLACK_BOT_TOKEN"]
 raise 'Missing ENV[SLACK_USER_TOKEN]!' unless ENV["SLACK_USER_TOKEN"]
+raise 'Missing ENV[SIGNING_SECRET]!' unless ENV["SIGNING_SECRET"]
