6e894d66208543b270bc00846328c5fb2bec0982
Amortya Ray <aray@handy.com>
2019-02-06 21:52:12 -0500

    Dynamic list of usrs

--- app/models/commands/open_hoop_dialog.rb
+++ app/models/commands/open_hoop_dialog.rb
@@ -15,68 +15,59 @@ module Commands
           "submit_label": "Add",
           "notify_on_cancel": false,
           "elements": [
-              {
-                  "type": "select",
-                  "label": "Type",
-                  "name": "type",
-                  "options": [
-                      {
-                        "label": "Out of Office",
-                        "value": "OOO"
-                      },
-                      {
-                        "label": "Out of Office - Half Day",
-                        "value": "HALF_OOO"
-                      },
-                      {
-                        "label": "Work from Home",
-                        "value": "WFH"
-                      },
-                      {
-                        "label": "Sick Day",
-                        "value": "SICK"
-                      },
-                      {
-                        "label": "Work from Home - Half Day",
-                        "value": "HALF_WFH"
-                      }
-                  ],
-              },
-              {
+            {
+                "type": "select",
+                "label": "Type",
+                "name": "type",
+                "options": [
+                    {
+                      "label": "Out of Office",
+                      "value": "OOO"
+                    },
+                    {
+                      "label": "Out of Office - Half Day",
+                      "value": "HALF_OOO"
+                    },
+                    {
+                      "label": "Work from Home",
+                      "value": "WFH"
+                    },
+                    {
+                      "label": "Sick Day",
+                      "value": "SICK"
+                    },
+                    {
+                      "label": "Work from Home - Half Day",
+                      "value": "HALF_WFH"
+                    }
+                ],
+            },
+            {
+              "type": "text",
+              "label": "What's this OOO for?",
+              "name": "description",
+              "placeholder": "Headed to Iceland!",
+              "optional": true,
+            },
+            {
                 "type": "text",
-                "label": "What's this OOO for?",
-                "name": "description",
-                "placeholder": "Headed to Iceland!",
+                "label": "First day you are OOO",
+                "name": "start_date",
+                "placeholder": "yyyy-mm-dd"
+            },
+            {
+                "type": "text",
+                "label": "Last day you are OOO",
+                "name": "end_date",
+                "placeholder": "yyyy-mm-dd",
                 "optional": true,
-              },
-              {
-                  "type": "text",
-                  "label": "First day you are OOO",
-                  "name": "start_date",
-                  "placeholder": "yyyy-mm-dd"
-              },
-              {
-                  "type": "text",
-                  "label": "Last day you are OOO",
-                  "name": "end_date",
-                  "placeholder": "yyyy-mm-dd",
-                  "optional": true,
-              },
-              {
-                  "type": "select",
-                  "label": "Approver",
-                  "name": "approver",
-                  "options": [
-                      {
-                        "label": "Asha B",
-                        "value": "ashab"
-                      },
-                      {
-                        "label": "Amortya Ray",
-                        "value": "morty"
-                      },
-                  ]
-              },
+            },
+            {
+                "type": "select",
+                "label": "Approver",
+                "name": "approver",
+                "data_source": "users"
+            }
           ]
         }
       }
